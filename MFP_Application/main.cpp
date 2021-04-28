5172bb8300b8ec712059c9a3b78a672c030c8f3e
joaquin aragon <theterx118@gmail.com>
2021-04-28 17:34:03 -0500

    first commit, just to check

---
+++ MFP_Application/main.cpp
@@ -0,0 +1,17 @@
+/* 
+ * File:   main.cpp
+ * Author: Joaquín Arturo Aragon Yauris
+ * Codigo: 20181252
+ * Created on 28 de abril de 2021, 05:32 PM
+ */
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    cout<<"hola mundo"<<endl;
+    return 0;
+}
+
